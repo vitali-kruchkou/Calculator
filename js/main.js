b10f8038b99142e43b758170c524ba241932dd68
Vitaliy Kruychkov <Kruychkov98@yandex.ru>
2021-07-06 14:42:50 +0300

    add history

--- js/main.js
+++ js/main.js
@@ -1,138 +1,175 @@
-let groups = document.querySelectorAll(".button-block")
- textarea = document.querySelector("#input-textarea")
- equally = document.querySelector(".button-operator-equally")
- btnDelete = document.querySelector(".button-operator-delete")
- btnBack = document.querySelector(".button-operator-back")
- sin = document.querySelector(".button-operator-sin")
- cos = document.querySelector(".button-operator-cos")
- tg = document.querySelector(".button-operator-tg")
- ctg = document.querySelector(".button-operator-ctg")
- gradus = document.querySelector("#gradus")
- radian = document.querySelector("#radian")
- radic = document.querySelector(".button-operator-radic")
- precent = document.querySelector('.button-operator-precent')
- hisory = document.querySelector('.history')
- ln = document.querySelector('.button-block-ln')
+let groups = document.querySelectorAll('.added ');
+textarea = document.querySelector('#input-textarea');
+equally = document.querySelector('.button-operator-equally');
+btnDelete = document.querySelector('.button-operator-delete');
+btnBack = document.querySelector('.button-operator-back');
+gradus = document.querySelector('#gradus');
+radian = document.querySelector('#radian');
+precent = document.querySelector('.button-operator-precent');
+hisory = document.querySelector('.history');
+complexFunction = document.querySelectorAll('.complex-function');
+historyTextList = document.createElement('ul');
 
-let historyText = document.createElement('div');
-
-groups.forEach((group) => {
-  group.addEventListener("click", () => {
+groups.forEach(group => {
+  group.addEventListener('click', () => {
     let chooseNumber = group.textContent;
     textarea.value += chooseNumber;
   });
 });
 
 function findFactorial(number) {
-  if(number == 0 || number == 1) {
+  if (number == 0 || number == 1) {
     return 1;
   } else {
     return number * findFactorial(number - 1);
   }
 }
 
-function outputInformation(equation,result) {
-  historyText.innerHTML(equation + ' = ' + result);
-}
-
 function computation() {
-  historyText.innerHTML += textarea.value + ' = ';
-  if (textarea.value.includes('%')){
+  let li = document.createElement('li');
+  li.innerHTML += textarea.value + ' = ';
+  if (textarea.value.includes('%')) {
     let wholePart = textarea.value.split('*')[0];
     let precent = textarea.value.split('*')[1].split('%')[0];
     textarea.value = wholePart * (precent / 100);
   }
-  if(textarea.value.indexOf('^') !== -1){
+  if (textarea.value.indexOf('^') !== -1) {
     let number = textarea.value.split('^')[0];
     let power = textarea.value.split('^')[1].split(' ')[0];
-    let operations = textarea.value.split(' ')[1]
-    let otherNumber =   textarea.value.split(' ')[2];
-    if (operations !== undefined && otherNumber !== undefined){
-      textarea.value = eval(Math.pow(number,power) + operations + otherNumber);
+    let operations = textarea.value.split(' ')[1];
+    let otherNumber = textarea.value.split(' ')[2];
+    if (operations !== undefined && otherNumber !== undefined) {
+      textarea.value = eval(Math.pow(number, power) + operations + otherNumber);
     } else {
-      textarea.value = Math.pow(number,power);
+      textarea.value = Math.pow(number, power);
     }
   }
-  if (textarea.value.indexOf('!') !== -1){
+  if (textarea.value.indexOf('!') !== -1) {
     let number = textarea.value.split('!')[0];
-    let operations = textarea.value.split(' ')[1]
-    let otherNumber =   textarea.value.split(' ')[2];
-    if (operations !== undefined && otherNumber !== undefined){
+    let operations = textarea.value.split(' ')[1];
+    let otherNumber = textarea.value.split(' ')[2];
+    if (operations !== undefined && otherNumber !== undefined) {
       textarea.value = eval(findFactorial(number) + operations + otherNumber);
     } else {
       textarea.value = findFactorial(number);
     }
   }
   textarea.value = eval(textarea.value);
-  historyText.innerHTML += textarea.value + '<br/>';
-  // hisory.appendChild(historyText); 
+  li.innerHTML += textarea.value;
+  historyTextList.appendChild(li);
 }
 
 function clearInput() {
-  textarea.value = "";
+  textarea.value = '';
 }
 
 function goBack() {
   textarea.value = textarea.value.substring(0, textarea.value.length - 1);
 }
 
-function findSin() {
-  if (gradus.checked === true) {
-    historyText.innerHTML += `${'Sin('+textarea.value+'°) = '}`;
-    textarea.value = Math.sin((textarea.value * Math.PI) / 180);
-    historyText.innerHTML += textarea.value + '<br/>';
-  } else if (radian.checked === true) {
-    historyText.innerHTML += `${'Sin(' + textarea.value + ' рад) = '}`;
-    textarea.value = Math.sin(textarea.value);
-    historyText.innerHTML += textarea.value + '<br/>';
+function findArg(e) {
+  let li = document.createElement('li');
+  li.innerHTML += `${e.target.textContent + '(' + textarea.value + ') = '}`;
+  switch (e.target.textContent) {
+    case 'sin':
+      if (gradus.checked === true) {
+        textarea.value = Math.sin((textarea.value * Math.PI) / 180);
+      } else if (radian.checked === true) {
+        textarea.value = Math.sin(textarea.value);
+      }
+      break;
+    case 'cos':
+      if (gradus.checked === true) {
+        textarea.value = Math.cos((textarea.value * Math.PI) / 180);
+      } else if (radian.checked === true) {
+        textarea.value = Math.cos(textarea.value);
+      }
+      break;
+    case 'tan':
+      if (gradus.checked === true) {
+        textarea.value = Math.tan((textarea.value * Math.PI) / 180);
+      } else if (radian.checked === true) {
+        textarea.value = Math.tan(textarea.value);
+      }
+      break;
+    case 'sinh':
+      if (gradus.checked === true) {
+        textarea.value = Math.sinh((textarea.value * Math.PI) / 180);
+      } else if (radian.checked === true) {
+        textarea.value = Math.sinh(textarea.value);
+      }
+      break;
+    case 'cosh':
+      if (gradus.checked === true) {
+        textarea.value = Math.cosh((textarea.value * Math.PI) / 180);
+      } else if (radian.checked === true) {
+        textarea.value = Math.cosh(textarea.value);
+      }
+      break;
+    case 'tanh':
+      if (gradus.checked === true) {
+        textarea.value = Math.tanh((textarea.value * Math.PI) / 180);
+      } else if (radian.checked === true) {
+        textarea.value = Math.tanh(textarea.value);
+      }
+      break;
+    case 'ctg':
+      if (gradus.checked === true) {
+        textarea.value = 1 / Math.tan((textarea.value * Math.PI) / 180);
+      } else if (radian.checked === true) {
+        textarea.value = 1 / Math.tan(textarea.value);
+      }
+      break;
+    case 'sec':
+      if (gradus.checked === true) {
+        textarea.value = 1 / Math.cos((textarea.value * Math.PI) / 180);
+      } else if (radian.checked === true) {
+        textarea.value = 1 / Math.cos(textarea.value);
+      }
+      break;
+    case 'csc':
+      if (gradus.checked === true) {
+        textarea.value = 1 / Math.sin((textarea.value * Math.PI) / 180);
+      } else if (radian.checked === true) {
+        textarea.value = 1 / Math.sin(textarea.value);
+      }
+      break;
+    case '√': {
+      textarea.value = Math.sqrt(textarea.value);
+      break;
+    }
+    case 'ln': {
+      textarea.value = Math.log(textarea.value);
+      break;
+    }
+    default:
+      alert('Please, enter data');
+      break;
   }
+  li.innerHTML += textarea.value;
+  historyTextList.appendChild(li);
 }
 
-function findCos() {
-  if (gradus.checked === true) {
-    textarea.value = Math.cos((textarea.value * Math.PI) / 180);
-  } else if (radian.checked === true) {
-    textarea.value = Math.cos(textarea.value);
-  }
-}
+complexFunction.forEach(button => {
+  button.addEventListener('click', findArg);
+});
 
-function findTan() {
-  if (gradus.checked === true) {
-    textarea.value = Math.tan((textarea.value * Math.PI) / 180);
-  } else if (radian.checked === true) {
-    textarea.value = Math.tan(textarea.value);
-  }
-}
-
-function findCtan() {
-  if (gradus.checked === true) {
-    textarea.value = 1 / Math.tan((textarea.value * Math.PI) / 180);
-  } else if (radian.checked === true) {
-    textarea.value = 1 / Math.tan(textarea.value);
-  }
-}
-
-function findLog() {
-  textarea.value = Math.log(textarea.value);
-}
-
-function findSqrt() {
-  textarea.value = Math.sqrt(textarea.value);
-}
-
-equally.addEventListener("click", computation);
-btnDelete.addEventListener("click", clearInput);
-btnBack.addEventListener("click", goBack);
-sin.addEventListener("click", findSin);
-cos.addEventListener("click", findCos);
-tg.addEventListener("click", findTan);
-ctg.addEventListener("click", findCtan);
-radic.addEventListener("click", findSqrt);
-ln.addEventListener('click',findLog);
-hisory.appendChild(historyText); 
+equally.addEventListener('click', computation);
+btnDelete.addEventListener('click', clearInput);
+btnBack.addEventListener('click', goBack);
+hisory.appendChild(historyTextList);
 
 document.addEventListener('keydown', event => {
-  if ((event.key).match(/=|Enter/)) {
+  let li = document.createElement('li');
+  if (event.key.match(/=|Enter/)) {
+    event.preventDefault();
+    li.innerHTML += textarea.value + ' = ';
     textarea.value = eval(textarea.value);
+    li.innerHTML += textarea.value;
+    historyTextList.appendChild(li);
   }
-})
+});
+
+historyTextList.addEventListener('click', e => {
+  textarea.value = e.target.textContent.split('=')[1];
+});
